d9bb7a815a4b4883a4211468ed7d2d8ed324096f
PuebloMillerini <44727319+PuebloMillerini@users.noreply.github.com>
2021-12-30 01:14:11 +0100

    Add files via upload

--- src/main/java/org/but/feec/footballdb/data/PersonRepository.java
+++ src/main/java/org/but/feec/footballdb/data/PersonRepository.java
@@ -10,6 +10,48 @@ import java.util.List;
 
 public class PersonRepository {
 
+    public void editPerson(PersonEditView personEditView) {
+        String insertPersonSQL = "UPDATE bds.person p SET email = ?, first_name = ?, nickname = ?, surname = ? WHERE p.id_person = ?";
+        String checkIfExists = "SELECT email FROM bds.person p WHERE p.id_person = ?";
+        try (Connection connection = DataSourceConfig.getConnection();
+             // would be beneficial if I will return the created entity back
+             PreparedStatement preparedStatement = connection.prepareStatement(insertPersonSQL, Statement.RETURN_GENERATED_KEYS)) {
+            // set prepared statement variables
+            preparedStatement.setString(1, personEditView.getEmail());
+            preparedStatement.setString(2, personEditView.getFirstName());
+            preparedStatement.setString(3, personEditView.getNickname());
+            preparedStatement.setString(4, personEditView.getSurname());
+            preparedStatement.setLong(5, personEditView.getId());
+
+            try {
+                // TODO set connection autocommit to false
+                /* HERE */
+                try (PreparedStatement ps = connection.prepareStatement(checkIfExists, Statement.RETURN_GENERATED_KEYS)) {
+                    ps.setLong(1, personEditView.getId());
+                    ps.execute();
+                } catch (SQLException e) {
+                    throw new DataAccessException("This person for edit do not exists.");
+                }
+
+                int affectedRows = preparedStatement.executeUpdate();
+
+                if (affectedRows == 0) {
+                    throw new DataAccessException("Creating person failed, no rows affected.");
+                }
+                // TODO commit the transaction (both queries were performed)
+                /* HERE */
+            } catch (SQLException e) {
+                // TODO rollback the transaction if something wrong occurs
+                /* HERE */
+            } finally {
+                // TODO set connection autocommit back to true
+                /* HERE */
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Creating person failed operation on the database failed.");
+        }
+    }
+
     public PersonAuthView findPersonByEmail(String email) {
 
         try (Connection connection = DataSourceConfig.getConnection();
@@ -30,44 +72,44 @@ public class PersonRepository {
         return null;
     }
 
-//    public UserDetailView findPersonDetailedView(Long user_id) {
-//        try (Connection connection = DataSourceConfig.getConnection();
-//             PreparedStatement preparedStatement = connection.prepareStatement(
-//                     "SELECT u.user_id, email, firstname, surname, username, city, house_number, street" +
-//                             "FROM public.user u" +
-//                             "LEFT JOIN public.user_has_address h ON u.user_id = h.user_id" +
-//                             "LEFT JOIN public.address a ON h.address_id = a.address_id" +
-//                             "WHERE u.user_id = ?;")
-//        ) {
-//            preparedStatement.setLong(1, user_id);
-//            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-//                if (resultSet.next()) {
-//                    return mapToUserDetailView(resultSet);
-//                }
-//            }
-//        } catch (SQLException e) {
-//            throw new DataAccessException("Find person by ID with addresses failed.", e);
-//        }
-//        return null;
-//    }
-//
-//    public List<UserBasicView> getPersonsBasicView() {
-//        try (Connection connection = DataSourceConfig.getConnection();
-//             PreparedStatement preparedStatement = connection.prepareStatement(
-//                     "SELECT u.user_id, email, firstname, surname, username, city" +
-//                             " FROM public.user u" +
-//                             " LEFT JOIN user_has_address h on u.user_id = h.user_id" +
-//                             " LEFT JOIN public.address a ON h.address_id = a.address_id;");
-//             ResultSet resultSet = preparedStatement.executeQuery();) {
-//            List<UserBasicView> userBasicViews = new ArrayList<>();
-//            while (resultSet.next()) {
-//                userBasicViews.add(mapToUserBasicView(resultSet));
-//            }
-//            return userBasicViews;
-//        } catch (SQLException e) {
-//            throw new DataAccessException("Persons basic view could not be loaded.", e);
-//        }
-//    }
+    public PersonDetailView findPersonDetailedView(Long user_id) {
+        try (Connection connection = DataSourceConfig.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "SELECT u.user_id, email, firstname, surname, username, city, house_number, street" +
+                             "FROM public.user u" +
+                             "LEFT JOIN public.user_has_address h ON u.user_id = h.user_id" +
+                             "LEFT JOIN public.address a ON h.address_id = a.address_id" +
+                             "WHERE u.user_id = ?;")
+        ) {
+            preparedStatement.setLong(1, user_id);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return mapToPersonDetailView(resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Find person by ID with addresses failed.", e);
+        }
+        return null;
+    }
+
+    public List<PersonBasicView> getPersonsBasicView() {
+        try (Connection connection = DataSourceConfig.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(
+                     "SELECT u.user_id, email, firstname, surname, username, city" +
+                             " FROM public.user u" +
+                             " LEFT JOIN user_has_address h on u.user_id = h.user_id" +
+                             " LEFT JOIN public.address a ON h.address_id = a.address_id;");
+             ResultSet resultSet = preparedStatement.executeQuery();) {
+            List<PersonBasicView> userBasicViews = new ArrayList<>();
+            while (resultSet.next()) {
+                userBasicViews.add(mapToPersonBasicView(resultSet));
+            }
+            return userBasicViews;
+        } catch (SQLException e) {
+            throw new DataAccessException("Persons basic view could not be loaded.", e);
+        }
+    }
 
     private PersonAuthView mapToUserAuth(ResultSet rs) throws SQLException {
         PersonAuthView person = new PersonAuthView();
@@ -75,4 +117,30 @@ public class PersonRepository {
         person.setPassword(rs.getString("password"));
         return person;
     }
+
+    private PersonDetailView mapToPersonDetailView(ResultSet rs) throws SQLException {
+        PersonDetailView personDetailView = new PersonDetailView();
+        personDetailView.setId(rs.getLong("id_person"));
+        personDetailView.setEmail(rs.getString("email"));
+        personDetailView.setGivenName(rs.getString("first_name"));
+        personDetailView.setFamilyName(rs.getString("surname"));
+        personDetailView.setNickname(rs.getString("nickname"));
+        personDetailView.setCity(rs.getString("city"));
+        personDetailView.sethouseNumber(rs.getString("house_number"));
+        personDetailView.setStreet(rs.getString("street"));
+        return personDetailView;
+    }
+
+    private PersonBasicView mapToPersonBasicView(ResultSet rs) throws SQLException {
+        PersonBasicView personBasicView = new PersonBasicView();
+        personBasicView.setId(rs.getLong("id_person"));
+        personBasicView.setEmail(rs.getString("email"));
+        personBasicView.setGivenName(rs.getString("first_name"));
+        personBasicView.setFamilyName(rs.getString("surname"));
+        personBasicView.setNickname(rs.getString("nickname"));
+        personBasicView.setCity(rs.getString("city"));
+        return personBasicView;
+    }
+
+
 }
--- src/main/java/org/but/feec/footballdb/services/PersonService.java
+++ src/main/java/org/but/feec/footballdb/services/PersonService.java
@@ -1,11 +1,26 @@
 package org.but.feec.footballdb.services;
+import org.but.feec.footballdb.api.PersonBasicView;
+import org.but.feec.footballdb.api.PersonDetailView;
+import org.but.feec.footballdb.api.PersonEditView;
 import  org.but.feec.footballdb.data.PersonRepository;
-import at.favre.lib.crypto.bcrypt.BCrypt;
+//import at.favre.lib.crypto.bcrypt.BCrypt;
+
+import java.util.List;
 
 public class PersonService {
     private PersonRepository personRepository;
     public PersonService(PersonRepository personRepository){
         this.personRepository = personRepository;
     }
+    public PersonDetailView getPersonDetailView(Long id) {
+        return personRepository.findPersonDetailedView(id);
+    }
+    public List<PersonBasicView> getPersonsBasicView() {
+        return personRepository.getPersonsBasicView();
+    }
+    public void editPerson(PersonEditView personEditView) {
+        personRepository.editPerson(personEditView);
+    }
+
 
 }
